36752f9c721fec656151556b04c992a625cc754a
Alfredo Delgado L <aadelop@gmail.com>
2024-09-27 15:26:30 +0200

    Added a small test with web3 library to check if working

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const {application } = require("express")
+const INFURA_MAINNET = "https://mainnet.infura.io/v3/75bc45b8eac4415497742ad30526545b"
+const { Web3 } = require('web3');
+const web3 = new Web3(INFURA_MAINNET)
 
 const ap = express()
-
 ap.listen(3333)
 
+BigInt.prototype.toJSON = function () {
+    return this.toString();
+};
+
 ap.get("/", async(req,res) =>{
-    res.send("hello from route /")
+    const bloque = await web3.eth.getBlockNumber();
+    res.send({bloque})
 })
