5e7798eca84be82091da733b1a112f882229e279
Alfredo Delgado L <aadelop@gmail.com>
2024-09-27 19:38:17 +0200

    Added basic react hook form to check wich services will be use the app block, balance or address

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,18 @@
 import {Outlet} from 'react-router-dom'
-
+import {useForm} from 'react-hook-form'
 
 export function Home (){
-    return <div>
-        <h1>Crypto explorer application</h1>
+    const {register, handleSubmit} = useForm()
+    const submitForm = (data) => {
+        console.log(data);
+    }
+
+    return <div className='container'>
+        <h1>Crypto explorer for Ethereum blockchain</h1>
+        <form action="" onSubmit={handleSubmit(submitForm)}>
+            <input {...register("data")} />
+            <button className='btn btn-primary'>Go</button>    
+        </form> 
         <Outlet/>
     </div>
 }
