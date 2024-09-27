846b506254a5904316bceb291f7b30251dca9161
Alfredo Delgado L <aadelop@gmail.com>
2024-09-27 19:05:24 +0200

    Configure react router and setting up the neccesary routes for the application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Home} from './components/Home'
 
 createRoot(document.getElementById('root')).render(
-  <h1>Initial react state</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<Home/>}></Route>
+      <Route path="/" element={<Home/>}>
+        <Route path="/block" element={<p>Block</p>}> </Route>
+        <Route path="/tx" element={<p>Transsactions</p>}></Route>
+        <Route path="/balance" element={<p>Balance</p>}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
