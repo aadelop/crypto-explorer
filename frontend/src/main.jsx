bf0179f599b2fb408ceb7e3f9e7cf162e1a59362
Alfredo Delgado L <aadelop@gmail.com>
2024-09-27 19:31:12 +0200

    Imported in the principal script the needed components created to interact with the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {Home} from './components/Home'
+import { Balance } from './components/Balance'
+import { Transaction } from './components/Transaction'
+import { Block } from './components/Block'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="*" element={<Home/>}></Route>
       <Route path="/" element={<Home/>}>
-        <Route path="/block" element={<p>Block</p>}> </Route>
-        <Route path="/tx" element={<p>Transsactions</p>}></Route>
-        <Route path="/balance" element={<p>Balance</p>}></Route>
+        <Route path="/block" element={<Block/>}> </Route>
+        <Route path="/tx" element={<Transaction />}></Route>
+        <Route path="/balance" element={<Balance/>}></Route>
       </Route>
     </Routes>
   </BrowserRouter>
