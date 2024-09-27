582c2b356e7c18bff8377b50e9f0ac986cf11160
Alfredo Delgado L <aadelop@gmail.com>
2024-09-27 18:14:05 +0200

    Added 3 new routes needed for the aplication of the explorer

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
 const express = require("express")
-const {application } = require("express")
+const {appplication } = require("express")
 const INFURA_MAINNET = "https://mainnet.infura.io/v3/75bc45b8eac4415497742ad30526545b"
 const { Web3 } = require('web3');
 const web3 = new Web3(INFURA_MAINNET)
 
-const ap = express()
-ap.listen(3333)
+const app = express()
+app.listen(3333)
 
 BigInt.prototype.toJSON = function () {
     return this.toString();
 };
 
-ap.get("/", async(req,res) =>{
+app.get("/", async(req,res) =>{
     const bloque = await web3.eth.getBlockNumber();
     res.send({bloque})
+})
+
+app.get("/block/:blockNumber", async(req,res) =>{
+    const block = await web3.eth.getBlock(req.params.blockNumber)
+    res.send(block)
+})
+
+app.get("/tx/:tx", async (req,res) => {
+    const tx = await web3.eth.getTransaction(req.params.tx)
+    res.send(tx)
+})
+
+app.get("/balance/:address", async (req,res) =>{
+    let  balance = await web3.eth.getBalance(req.params.address)
+    balance = Number(balance)
+    res.send({balance, ethers:balance / 1e18, ethers2:web3.utils.fromWei(balance,'ether')})
 })
