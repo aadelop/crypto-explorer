733c99469ce676e0063196c7cbb165c867208cce
Alfredo Delgado L <aadelop@gmail.com>
2024-09-30 18:35:42 +0200

    Moved api request function to specific file

---
+++ frontend/src/components/api.jsx
@@ -0,0 +1,28 @@
+export async function getBlock(block){
+    const response = fetch(`http://localhost:3333/block/${block.queryKey[1]}`)
+     .then(response => response.json())
+     .then(data => {
+         return data
+     });
+     return response;
+ }
+
+ export async function getTx(tx){
+    const response = fetch(`http://localhost:3333/tx/${tx.queryKey[1]}`)
+     .then(response => response.json())
+     .then(data => {
+         return data
+     });
+     return response;
+ }
+
+ export async function getBalance(balance){
+    console.log("balance of getBalance:", balance);
+    const response = fetch(`http://localhost:3333/balance/${balance.queryKey[1]}`)
+     .then(response => response.json())
+     .then(data => {
+         return data
+     });
+     return response;
+ }
+
