b1a377e7f9fb8caf2ccc853f19f7ee5eec62e884
Alfredo Delgado L <aadelop@gmail.com>
2024-09-30 17:54:29 +0200

    Adapted main script to manage react-query library for the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,27 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {Home} from './components/Home'
 import { Balance } from './components/Balance'
 import { Transaction } from './components/Transaction'
 import { Block } from './components/Block'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<Home/>}></Route>
-      <Route path="/" element={<Home/>}>
-        <Route path="/block/:block" element={<Block/>}> </Route>
-        <Route path="/tx/:tx" element={<Transaction />}></Route>
-        <Route path="/balance/:address" element={<Balance/>}></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client = {queryClient}>
+     <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Home/>}></Route>
+        <Route path="/" element={<Home/>}>
+          <Route path="/block/:block" element={<Block/>}> </Route>
+          <Route path="/tx/:tx" element={<Transaction />}></Route>
+          <Route path="/balance/:address" element={<Balance/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
+   
+ 
 )
