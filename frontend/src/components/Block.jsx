7cea58590e3b8029c9b58f7dd8c92659f38d017f
Alfredo Delgado L <aadelop@gmail.com>
2024-09-30 17:53:47 +0200

    Completed the logic to get the info of block from server section with react-query component

--- frontend/src/components/Block.jsx
+++ frontend/src/components/Block.jsx
@@ -1,10 +1,33 @@
 import { useParams } from "react-router-dom"
+import {useQuery} from 'react-query'
+
+async function getBlock(block){
+   const response = fetch(`http://localhost:3333/block/${block.queryKey[1]}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return data
+    });
+
+    return response;
+    //.then(response => response.body)
+   /*  const data = await response
+    const reader = data.body.getReader()
+    console.log("Reader:", reader) */
+   /*  return data */
+}
 
 export function Block(){
     const params = useParams()
+    const {isLoading, isError, data} = useQuery(['block',params.block], getBlock)
+    
+    if(isLoading)
+        return <h3>Cargando...</h3>
+    if(isError)
+        return <h3>Error</h3>
 
     return <div className="container">
         <p>Blocks</p>
-        <p> {params.block}</p>
+        <pre> {JSON.stringify(data,null,4)}</pre>
     </div>
 }
